438f3dc23f49ebe52a784faecae132a6ca4a30bf
supergajit2 <supergajit1@comcast.net>
2021-07-09 13:46:03 -0600

    imzt requirement, mtc report updates

--- cypress/integration/MTC-Reports.js
+++ cypress/integration/MTC-Reports.js
@@ -1,85 +1,48 @@
 // <reference types = "Cypress" />
 
-describe('Run all the MTC Reports', () => {
+describe('Run all the MTC Reports and check numbers', () => {
+    before(() => {
+        cy.visit(Cypress.env("testAdminHome"))
+        cy.fixture('users').then(function(user) {
+            globalThis.user = user
+        })
+        cy.fixture('MTC').then(function(mtc) {
+            globalThis.mtc = mtc
+        })
+    })
     beforeEach(() => {
         Cypress.Cookies.preserveOnce(...["ChurchSSO-int", "JSESSIONID", "__VCAP_ID__"])
     })
     it('Successful login', () => {
-        cy.visit('https://missionary-stage.churchofjesuschrist.org/portal/admin-home?lang=eng')
-        cy.get('#username').type('imt23')
-        cy.get('#password').type('password1')
-        cy.get('#sign-in').click()
+        cy.login(user.imt23, user.password)
         cy.get('[data-testid="title-text"]').contains('Missionary Immunization Search')
     });    
-    it('Run Columbia/Venezuela', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Colombia Missionary Training Center- Venezuela Satellite"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-        cy.get('[data-testid="week-summary"]').contains('Younger')
-    });
-    it('Run Peru Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Perú Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run S. Africa Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="South Africa Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run Columbia Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Colombia Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run Ghana Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Ghana Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run Provo Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Provo Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run Brazil Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Brazil Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run Mexico Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Mexico Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run England Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="England Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run New Zealand Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="New Zealand Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    it('Run Phillippines Remote', () => {
-        cy.get('[data-testid="nav-menu"]').click()
-        cy.get('[name="MTC List"]').click()
-        cy.get('[name="Philippines Missionary Training Center- Remote Training"]').click() 
-        cy.get('[data-testid="subtitle-text"]').contains('Summary Report')
-    });
-    /* it('Logout', () => {
-        cy.get('[data-testid="text-with-icon"]').contains('24').click()
-        cy.get('[data-testid="link"]').contains('Sign Out').click()
-    }) */
+    it("Run all reports and confirm # of missionary Rows", () => {
+        
+        globalThis.mtc.MTC.forEach(function(element) {
+            cy.get('[data-testid="nav-menu"]').click()
+            cy.get('[name="MTC List"]').click()
+            cy.get('[name="' + element + '"]').click()
+            //cy.log(element)
+            cy.get('[data-testid="title-text"]').contains(element)
+            cy.get(':nth-child(1) > .WeekSummary__TypeCount-sc-1wgr37a-5').then(($younger) => {
+                const younger = $younger.text()
+                //cy.log(younger)
+                cy.get(':nth-child(3) > .WeekSummary__TypeCount-sc-1wgr37a-5').then(($senior) => {
+                    const senior = $senior.text()
+                    //cy.log(senior)
+                    const reporting = (+younger + +senior)
+                    //cy.log(reporting)
+                    if(reporting>0) {
+                        cy.get('.MissionaryRow__A11yStyled-sc-1jdo08v-2 > .details > .sc-1bujx24-0 > :nth-child(1)').then(missionaryRow => {
+                            const count = Cypress.$(missionaryRow).length
+                            expect(reporting).to.eq(count)
+                        })
+                    }
+                })
+                
+            })
+            
+        })
+    })
 })
